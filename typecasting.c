1a63a4129a706a75cf0de7aa20f53c4e7cacd4a4
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-20 15:42:50 +0300

    loops,type casting and sizeof

---
+++ typecasting.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num, num1;
+    num = 20;
+    num1 = 3;
+
+    float division = (float)num / (float)num1;
+    printf("%f\n", division);
+    return 0;
+}
